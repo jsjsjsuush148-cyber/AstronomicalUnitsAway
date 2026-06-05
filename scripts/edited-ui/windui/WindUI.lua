ce91940bf52070e4bd5873ea2c10c2ca8fe21464
jsjsjsuush148-cyber <jsjsjsuush148@gmail.com>
2026-06-05 17:48:12 -0600

    Update WindUI.lua

--- scripts/edited-ui/windui/WindUI.lua
+++ scripts/edited-ui/windui/WindUI.lua
@@ -11962,7 +11962,7 @@ Parent=am.UIElements.TabsScroll,
 },{
 aa.NewRoundFrame(math.max(al.Window.ElementConfig.UICorner-2,6),al.Window.ModernLayout and"Glass-1.4"or"SquircleOutline",{
 Size=UDim2.new(1,0,1,0),
-BackgroundTransparency=,
+BackgroundTransparency=1,
 ThemeTag={
 ImageColor3="TabBorder",
 },
